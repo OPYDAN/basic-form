34d5de2f8d52fddddbfc62fa5243002dff548857
OPYDAN <29649698+OPYDAN@users.noreply.github.com>
2025-03-18 01:56:03 +0100

    Updated HTML and CSS with Javascript
    Ensuring that the page is responsive, the register link takes you to a new page linked to the form and ensuring that the details you input is displayed for confirmation.

--- basicform.js
+++
@@ -1,5 +0,0 @@
-function handleSubmit(event) {
-    event.preventDefault(); // Prevent actual form submission
-    document.getElementById("form-heading").innerText = "Welcome";
-    alert("You clicked this button");
-}
